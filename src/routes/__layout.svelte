4126e334a323bd6b2164d93370069e5c60d2c7ee
Dheepak Krishnamurthy <me@kdheepak.com>
2022-02-15 16:24:27 -0700

    new: Initial commit 🎉

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,18 @@
+<script>
+  import '../app.css'
+
+  import Navbar from '$lib/Navbar.svelte'
+</script>
+
+<div class="h-screen w-screen grid">
+  <Navbar />
+  <main>
+    <slot />
+  </main>
+</div>
+
+<style>
+  div {
+    grid-template-rows: auto 1fr;
+  }
+</style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}', './node_modules/tw-elements/dist/js/**/*.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('tw-elements/dist/plugin')],
+}
