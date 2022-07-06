381b51407b9510dfd831da1610b10b448cee0725
Dheepak Krishnamurthy <me@kdheepak.com>
2022-07-06 17:53:26 -0600

    chore: Update dependencies ♻️

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
-import preprocess from 'svelte-preprocess'
-import adapter from '@sveltejs/adapter-static'
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,19 +8,12 @@ const config = {
   preprocess: preprocess({ postcss: true }),
 
   kit: {
-    vite: {
-      resolve: {
-        alias: {
-          path: 'path-browserify',
-        },
-      },
-    },
     adapter: adapter({
       pages: 'build',
       assets: 'build',
-      fallback: 'index.html',
-    }),
-  },
-}
+      fallback: 'index.html'
+    })
+  }
+};
 
-export default config
+export default config;
