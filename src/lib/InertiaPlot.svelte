7f3f3bd9161991b42f8295062bd68bbae43ae2b3
Dheepak Krishnamurthy <me@kdheepak.com>
2022-07-06 22:43:46 -0600

    feat: Show only last 10 elements ✨

--- src/lib/InertiaPlot.svelte
+++ src/lib/InertiaPlot.svelte
@@ -3,78 +3,79 @@
   Generates an SVG area shape using the `area` function from [d3-shape](https://github.com/d3/d3-shape).
  -->
 <script lang="ts">
-  import * as d3 from 'd3'
-  import { LayerCake, ScaledSvg, Svg, Html } from 'layercake'
-  import { getContext } from 'svelte'
-  const { data, width, height } = getContext('LayerCake')
-  export let stroke = '#ab00d6'
+  import * as d3 from "d3";
+  import { LayerCake, ScaledSvg, Svg, Html } from "layercake";
+  import { getContext } from "svelte";
+  const { data, width, height } = getContext("LayerCake");
+  export let stroke = "#ab00d6";
+  export let inertiaFloorStroke = "#ff0000";
 
-  $: console.log($data)
+  $: console.log($data);
   $: timeScale = d3
     .scaleTime()
     .domain(
-      d3.extent($data.commitment_data, (d) => {
-        let t = new Date(d.timestamp)
-        t.setTime(t.getTime() + t.getTimezoneOffset() * 60 * 1000)
-        return t
+      d3.extent($data.commitment_data.slice(-10), (d) => {
+        let t = new Date(d.timestamp);
+        t.setTime(t.getTime() + t.getTimezoneOffset() * 60 * 1000);
+        return t;
       }),
     )
-    .range([0, $width])
+    .range([0, $width]);
   $: inertiaScale = d3
     .scaleLinear()
-    .domain(d3.extent($data.commitment_data, (d) => inertiaRow(d)))
-    .range([$height, 0])
+    .domain([0, d3.extent($data.commitment_data, (d) => inertiaRow(d))[1]])
+    .range([$height, 0]);
 
   function inertiaRow(d) {
-    let h = 0
+    let h = 0;
     for (const gen of $data.generator_data) {
-      h += d[gen.name] * gen.h * gen.mva
+      h += d[gen.name] * gen.h * gen.mva;
     }
-    return h
+    return h;
   }
 
-  let xaxis = null
-  let yaxis = null
-  $: d3.select(xaxis).call(d3.axisBottom(timeScale).tickFormat(d3.timeFormat('%H:%M:%S')))
-  $: d3.select(yaxis).call(d3.axisLeft(inertiaScale))
+  let xaxis = null;
+  let yaxis = null;
+  $: d3.select(xaxis).call(d3.axisBottom(timeScale).tickFormat(d3.timeFormat("%H:%M:%S")));
+  $: d3.select(yaxis).call(d3.axisLeft(inertiaScale));
 
   function getLinePlot(data, width) {
-    const lineplot = []
-    for (const row of data.commitment_data) {
-      let h = 0
+    const lineplot = [];
+    for (const row of data.commitment_data.slice(-10)) {
+      let h = 0;
       for (const gen of data.generator_data) {
-        h += row[gen.name] * gen.h * gen.mva
+        h += row[gen.name] * gen.h * gen.mva;
       }
-      let d = new Date(row.timestamp)
-      d.setTime(d.getTime() + d.getTimezoneOffset() * 60 * 1000)
-      lineplot.push({ timestamp: timeScale(d), value: inertiaScale(h) })
+      let d = new Date(row.timestamp);
+      d.setTime(d.getTime() + d.getTimezoneOffset() * 60 * 1000);
+      lineplot.push({ timestamp: timeScale(d), value: inertiaScale(h) });
     }
-    return lineplot
+    return lineplot;
   }
 
   function getInertiaFloorLinePlot(data, width) {
-    const lineplot = []
-    for (const row of data.commitment_data) {
-      let d = new Date(row.timestamp)
-      d.setTime(d.getTime() + d.getTimezoneOffset() * 60 * 1000)
-      lineplot.push({ timestamp: timeScale(d), value: inertiaScale(data.inertia_floor) })
+    const lineplot = [];
+    for (const row of data.commitment_data.slice(-10)) {
+      let d = new Date(row.timestamp);
+      d.setTime(d.getTime() + d.getTimezoneOffset() * 60 * 1000);
+      lineplot.push({ timestamp: timeScale(d), value: inertiaScale(data.inertia_floor) });
     }
-    return lineplot
+    return lineplot;
   }
   $: path =
-    'M' +
+    "M" +
     getLinePlot($data, $width)
       .map((d) => {
-        return d.timestamp + ',' + d.value
+        return d.timestamp + "," + d.value;
       })
-      .join('L')
+      .join("L");
   $: inertiaFloorPath =
-    'M' +
+    "M" +
     getInertiaFloorLinePlot($data, $width)
       .map((d) => {
-        return d.timestamp + ',' + d.value
+        return d.timestamp + "," + d.value;
       })
-      .join('L')
+      .join("L");
 </script>
 
 <Svg>
@@ -106,6 +107,6 @@
       </text>
     </g>
     <path class="path-line" fill="none" d={path} {stroke} />
-    <path class="path-line" fill="none" d={inertiaFloorPath} {stroke} />
+    <path class="path-line" fill="none" d={inertiaFloorPath} stroke={inertiaFloorStroke} />
   </g>
 </Svg>
